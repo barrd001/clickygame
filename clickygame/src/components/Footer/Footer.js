9ee8a4074f0efe8928b3aa6ca3727dc13285ef1b
Devyn Barron <52061626+barrd001@users.noreply.github.com>
2020-01-19 16:57:40 -0800

    updated footer

--- clickygame/src/components/Footer/Footer.js
+++ clickygame/src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return(
-  <footer className='sticky'>
+  <footer className='container'>
     NCAAF Clicky Game by Devyn Barron
   </footer>
   )
